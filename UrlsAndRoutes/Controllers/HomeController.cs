73c70b42f3029e9c5d829ad3b91ea9b42c5f81e0
Darren Seet Serkai <darren@seet.me>
2013-12-17 18:03:41 +0900

    Chapter 13 initial commit

---
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "Home";
+            ViewBag.Action = "Index";
+            return View("ActionName");
+        }
+    }
+}
