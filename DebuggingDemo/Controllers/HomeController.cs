560718e099c6e320af7c87f6842996a7fedfd422
Darren Seet Serkai <darren@seet.me>
2013-12-17 17:13:15 +0900

    Completed chapter 13

---
+++ DebuggingDemo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DebuggingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var firstVal = 10;
+            var secondVal = 5;
+            var result = firstVal / secondVal;
+            //ViewBag.Message = "Welcome to ASP.NET MVC!";
+            return View(result);
+        }
+
+    }
+}
