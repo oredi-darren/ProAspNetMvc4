585e56bb2892c17a4cb6ff8ad88d0e85c778d842
Darren Seet Serkai <darren@seet.me>
2013-12-27 17:46:02 +0900

    custom redirect result

---
+++ ControllersAndActions/Controllers/BasicController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ControllersAndActions.Controllers
+{
+    public class BasicController
+        : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            var controller = requestContext.RouteData.Values["controller"] as string;
+            var action = requestContext.RouteData.Values["action"] as string;
+
+            if (action.ToLower() == "redirect")
+            {
+                requestContext.HttpContext.Response.Redirect("/Derived/Index");
+            }
+            else
+            {
+                requestContext.HttpContext.Response.Write(string.Format("Controller: {0}, Action: {1}", controller, action));
+            }
+        }
+    }
+}
---
+++ ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,34 @@
+﻿using ControllersAndActions.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        //
+        // GET: /Derived/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello from the DerivedController Index method";
+            return View("MyView");
+        }
+
+        public ActionResult ProduceOutput()
+        {
+            if (Server.MachineName.ToLower() == "darren-windows")
+            {
+                return new CustomRedirectResult { Url = "/Basic/Index" };
+            }
+            else
+            {
+                Response.Write("Controller: Derived, Action: ProductOutput");
+                return null;
+            }
+        }
+    }
+}
