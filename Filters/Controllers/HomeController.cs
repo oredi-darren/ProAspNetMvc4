04c3c0feccd26ff6d162f65e04a4fd0a3fcb6aa9
Darren Seet Serkai <darren@seet.me>
2013-12-28 17:45:30 +0900

    Completed Exception filter

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Filters.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [CustomAuth(true)]
+        public string Index()
+        {
+            return "This is the Index action on the Home controller";
+        }
+
+        [HandleError(ExceptionType = typeof(ArgumentOutOfRangeException), View="RangeError")]
+        public string RangeTest(int id)
+        {
+            if (id <= 100)
+            {
+                throw new ArgumentOutOfRangeException("id", id, "");
+            }
+
+            return string.Format("The id value is: {0}", id);
+        }
+    }
+}
---
+++ Filters/Infrastructure/CustomAuthAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+    public class CustomAuthAttribute
+        : AuthorizeAttribute
+    {
+        private bool _localAllowed;
+        public CustomAuthAttribute(bool localAllowed)
+        {
+            _localAllowed = localAllowed;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Request.IsLocal)
+            {
+                return _localAllowed;
+            }
+            return true;
+        }
+    }
+}
