65f8e0b8d318646a5ff2e070ad2c02866fd82e38
Darren Seet Serkai <darren@seet.me>
2014-01-02 17:39:20 +0900

    Completed chapter 20

---
+++ TemplatedHelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+using TemplatedHelperMethods.Models;
+
+namespace TemplatedHelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new[] { "Apple", "Orange", "Pear" };
+            ViewBag.Cities = new[] { "New York", "London", "Paris" };
+
+            var message = "This is an HTML element: <input>";
+            return View(message as Object);
+        }
+
+        public ActionResult CreatePerson()
+        {
+            return View(new Person { IsApproved = true });
+        }
+
+        [HttpPost]
+        public ActionResult CreatePerson(Person person)
+        {
+            return View("DisplayPerson", person);
+        }
+
+        public ActionResult ModelCreatePerson()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult ModelCreatePerson(Person person)
+        {
+            return View("DisplayPerson", person);
+        }
+    }
+}
